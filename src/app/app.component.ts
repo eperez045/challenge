5fa5ff433572839bbf0bef9af789a4c9ee8fa58e
eperez045 <eperez045@gmail.com>
2021-07-22 19:02:26 +0200

    correciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   episodes: any[] = [];
 
+  itemsPerPage: number = 10;
+  allPages!: number;
 
 
   constructor(private http: HttpClient){
 
-    
-    this.http.get('https://rickandmortyapi.com/api/episode').subscribe((data: any)=> {
-                      console.log(data.results)
-                      this.episodes = data.results})
   }
 
+  ngOnInit() {
+    this.http.get('https://rickandmortyapi.com/api/episode').subscribe((data: any)=> {
+      console.log(data.results)
+      this.episodes = data.results})
+}
+onPageChange(page: number = 1): void {
+  const startItem = (page - 1) * this.itemsPerPage;
+  const endItem = page * this.itemsPerPage;
+  this.episodes = this.episodes.slice(startItem, endItem);
+}
 
 
   title = 'prueba';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
